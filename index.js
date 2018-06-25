b580756d445aa2faf40bdd6251335c117a9e0e3d
Justin Hall <hall.justin.d@gmail.com>
2018-06-25 18:59:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,25 @@ class BoardMember{
       return 'No, I must disagree';
     }
     
+    approve(){
+      return 'You can do that!'
+    }
     
+    doCharity(){
+      return 'I like to help people.'
+    }
+    
+    releasePressStatement(){
+      return 'You will see great things from Scuber.'
+    }
+    
+    sayHi(){
+      return `Hi, my name is ${this.name}. I am from ${homestate}, and I was trained in ${training}.`
+    }
+}
+
+class Ceo extends BoardMember{
+  hireEmployee(){
+    return 'Welcome aboard!'
+  }
 }
